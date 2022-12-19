e6c1d320911ef62fa08b22aba12020c543625e07
Vincent Houillon <vhouillon@gmail.com>
2022-12-19 11:24:44 +0100

    add register, login, logout user

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,4 +1,4 @@
-"""config URL Configuration
+"""accounts URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.1/topics/http/urls/
@@ -9,14 +9,16 @@ Function views
 Class-based views
     1. Add an import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+
+from .views import *
+
+app_name = "accounts"
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("accounts/", include("accounts.urls")),
+    path("", HomepageView.as_view(), name="home"),
+    path("register/", UserRegsiterView.as_view(), name="register"),
+    path("login/", UserLoginView.as_view(), name="login"),
+    path("logout/", UserLogoutView.as_view(), name="logout"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,28 @@
+from django.contrib.auth.views import LoginView, LogoutView
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views.generic import CreateView, TemplateView
 
-# Create your views here.
+from .forms import UserLoginForm, UserRegisterForm
+from .models import User
+
+
+class HomepageView(TemplateView):
+    template_name = "registration/homepage.html"
+
+
+class UserRegsiterView(CreateView):
+    model = User
+    form_class = UserRegisterForm
+    template_name = "registration/register.html"
+    success_url = reverse_lazy("accounts:home")
+
+
+class UserLoginView(LoginView):
+    authentication_form = UserLoginForm
+    template_name = "registration/login.html"
+    next_page = reverse_lazy("accounts:home")
+
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy("accounts:home")
