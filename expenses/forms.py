58540fd503d8d47d60f4e27c467833aa2956e3bc
Vincent Houillon <vhouillon@gmail.com>
2023-01-05 18:08:30 +0100

    add style css for supplies & expenses form

--- expenses/forms.py
+++ expenses/forms.py
@@ -1,18 +1,44 @@
-from django import forms
+import datetime
 
-from .models import Expense
+from django import forms
+from django.utils.translation import gettext_lazy as _
+
+from .models import CATEGORIES, Expense
 
 
 class ExpenseForm(forms.ModelForm):
+    category = forms.CharField(
+        label=_("Category"), widget=forms.Select(choices=CATEGORIES)
+    )
+    date = forms.DateField(
+        label=_("Date"),
+        initial=datetime.date.today,
+        widget=forms.TextInput(attrs={"type": "date"}),
+    )
+    title = forms.CharField(label=_("Title"))
+    vendor = forms.CharField(label=_("Vendor"), required=False)
+    price = forms.DecimalField(
+        label=_("Total"),
+        initial=0.0,
+        decimal_places=2,
+        widget=forms.TextInput(attrs={"type": "number", "step": "any"}),
+    )
+    mileage = forms.IntegerField(
+        label=_("Mileage"),
+        required=False,
+        widget=forms.TextInput(attrs={"type": "number"}),
+    )
+    note = forms.CharField(
+        label=_("Note"), required=False, widget=forms.Textarea(attrs={"rows": 6})
+    )
+    attachment = forms.FileField(label=_("File"), required=False)
+
     class Meta:
         model = Expense
-        fields = (
-            "title",
-            "vendor",
-            "category",
-            "date",
-            "mileage",
-            "price",
-            "attachment",
-            "note",
-        )
+        fields = "__all__"
+        exclude = ("vehicle",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control form-control-sm"
--- supplies/forms.py
+++ supplies/forms.py
@@ -1,19 +1,35 @@
+import datetime
+
 from django import forms
+from django.utils.translation import gettext_lazy as _
 
 from .models import Supply
 
 
 class SupplyForm(forms.ModelForm):
+    date = forms.DateField(
+        label=_("Date"),
+        initial=datetime.date.today,
+        widget=forms.TextInput(attrs={"type": "date"}),
+    )
+
+    attachment = forms.FileField(label=_("File"), required=False)
+    full_tank = forms.BooleanField(
+        label=_("full tank"),
+        required=False,
+        widget=forms.CheckboxInput(
+            attrs={"class": "form-check-input", "type": "checkbox"}
+        ),
+    )
+    note = forms.CharField(
+        label=_("Note"), required=False, widget=forms.Textarea(attrs={"rows": 6})
+    )
+
     class Meta:
         model = Supply
-        fields = (
-            "vendor",
-            "fueltype",
-            "date",
-            "mileage",
-            "number_of_liters",
-            "price",
-            "full_tank",
-            "attachment",
-            "note",
-        )
+        exclude = ("vehicle",)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        for field in self.fields.values():
+            field.widget.attrs["class"] = "form-control form-control-sm"
