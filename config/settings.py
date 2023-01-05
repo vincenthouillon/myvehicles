5698073a3b19885a59c6e6d0b921b4b7e4235901
Vincent Houillon <vhouillon@gmail.com>
2023-01-05 11:03:41 +0100

    add boostrap, bootstrap-icons, chart.js, sass...

--- config/settings.py
+++ config/settings.py
@@ -125,6 +125,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATICFILES_DIRS = [BASE_DIR / "static"]
 
 PRIVATE_STORAGE_ROOT = "./media/"
 PRIVATE_STORAGE_AUTH_FUNCTION = "private_storage.permissions.allow_authenticated"
